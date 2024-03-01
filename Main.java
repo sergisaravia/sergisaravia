806f717cbc5555421142aae3eeabdf9f6cf6c003
sergisaravia <157466947+sergisaravia@users.noreply.github.com>
2024-03-01 22:59:41 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+    public static int [][] crearTablaSudoku () {
+        int [][] tablaSudoku = new int[9][9];
+        return tablaSudoku;
+    }
+
+    public static int [][] crearCuadradoSudoku () {
+        Random random = new Random();
+        int [][] cuadradoSudoku = new int[3][3];
+        for (int i = 0; i < 9; ++i) {
+            for (int j = 0; i < 9; ++j) {
+
+            }
+        }
+        return cuadradoSudoku;
+    }
+}
